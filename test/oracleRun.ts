5dc3a7ad6ab01e82feec4c46d0925dc52449df0d
Alex Sumner <alex_sumner@mentor.com>
2021-12-08 09:54:36 +0000

    fetching COMP rewards

--- test/oracleRun.ts
+++ test/oracleRun.ts
@@ -8,9 +8,12 @@ const compAbi = require("../external/compound.json");
 const aaveAbi = require("../external/aave.json");
 const ERC20 = require("../external/erc20.json");
 const aaveLendingPoolAbi = require('../external/aaveLendingPool.json');
+const comptrollerAbi = require('../external/comptroller.json');
+
 
 const USDC_MAINNET = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
-const STRIPS_TEST = "0x21d0fd7c54e4c1A453906A3BcB7598aeE827b292";
+const STRIPS_AAVE_WALLET = "0x21d0fd7c54e4c1A453906A3BcB7598aeE827b292";
+const STRIPS_COMPOUND_WALLET = "0xD7CFA9A98e0BDFDAcB5D7e07FBe9E528F057984D";
 
 describe("Compound APY", function() {
 
@@ -21,41 +24,46 @@ describe("Compound APY", function() {
     before(async function() {
         [this.deployer] = await ethers.getSigners();
         this.USDC = new ethers.Contract(USDC_MAINNET, ERC20, this.deployer);
-        await ethers.provider.send("hardhat_impersonateAccount", [STRIPS_TEST]);
-        this.stripsSigner = await ethers.getSigner(STRIPS_TEST);
     });
 
     it("estimate USDC supply APY, Compound", async function() {
+        await ethers.provider.send("hardhat_impersonateAccount", [STRIPS_COMPOUND_WALLET]);
+        const stripsCompoundSigner = await ethers.getSigner(STRIPS_COMPOUND_WALLET);
         const cUSDC = new ethers.Contract(Contracts.cUSDC, compAbi["cErc20Delegate"], this.deployer);
         const compContract = new ethers.Contract(Contracts.COMP, compAbi["COMP"], this.deployer);
+        const ComptrollerContract = new ethers.Contract(Contracts.Comptroller, comptrollerAbi, this.deployer);
 
         const lastResult = compoundResults.at(-1);
         const prevBalance_d18 = lastResult ? ethers.utils.parseUnits(lastResult.balance, 18) : ZERO;
-        const prevCompReward_d18 = lastResult ? ethers.utils.parseUnits(lastResult.compRewardBalance, 18) : ZERO;
+        const prevCompReward = lastResult ? ethers.utils.parseUnits(lastResult.compRewardBalance, 18) : ZERO;
+
         const prevTime = lastResult ? lastResult.time : 0;
 
-        const usdcBeforeWithdrawal = await this.USDC.balanceOf(STRIPS_TEST);
+        const usdcBeforeWithdrawal = await this.USDC.balanceOf(STRIPS_COMPOUND_WALLET);
 
-        const ctokenBalance = await cUSDC.balanceOf(STRIPS_TEST);
-        const tx = await cUSDC.redeem(ctokenBalance);
+        let tx = await ComptrollerContract.connect(stripsCompoundSigner).claimComp(STRIPS_COMPOUND_WALLET, [cUSDC.address]);
+        const ctokenBalance = await cUSDC.balanceOf(STRIPS_COMPOUND_WALLET);
+        tx = await cUSDC.connect(stripsCompoundSigner).redeem(ctokenBalance);
         const currentTime = Date.now();
         await tx.wait();
 
-        const usdcAfterWithdrawal = await this.USDC.balanceOf(STRIPS_TEST);
+        const usdcAfterWithdrawal = await this.USDC.balanceOf(STRIPS_COMPOUND_WALLET);
         const currentBalance = usdcAfterWithdrawal.sub(usdcBeforeWithdrawal);
         const currentBalance_d18 = currentBalance.mul(ethers.BigNumber.from(10).pow(12));
 
-        const currentCompReward = await compContract.balanceOf(STRIPS_TEST);
+        const currentCompReward = await compContract.balanceOf(STRIPS_COMPOUND_WALLET);
         const chainLinkCompUSD = new ethers.Contract(Contracts.ChainLinkCompUSD, compAbi["ChainLinkCompUSD"], this.deployer);
 
         const compPriceData = await chainLinkCompUSD.latestRoundData();
         const currentCompPrice = compPriceData.answer;
-        const currentCompReward_d18 = currentCompReward.mul(currentCompPrice).div(ethers.BigNumber.from(10).pow(8));
+        const compRewardProfit_d18 = currentCompReward.sub(prevCompReward).mul(currentCompPrice).div(ethers.BigNumber.from(10).pow(8));
+        console.log("currentCompReward " + ethers.utils.formatUnits(currentCompReward, 18));
+        console.log("compRewardProfit_d18 " + ethers.utils.formatUnits(compRewardProfit_d18, 18));
 
-        const currentProfit = currentBalance_d18.sub(prevBalance_d18).add(currentCompReward_d18.sub(prevCompReward_d18));
+        const currentProfit = currentBalance_d18.sub(prevBalance_d18).add(compRewardProfit_d18);
         const profitStr = ethers.utils.formatUnits(currentProfit, 18);
         const balanceStr = ethers.utils.formatUnits(currentBalance_d18, 18);
-        const rewardStr = ethers.utils.formatUnits(currentCompReward_d18, 18);
+        const rewardStr = ethers.utils.formatUnits(currentCompReward, 18);
         const priceStr = ethers.utils.formatUnits(currentCompPrice, 8);
         const interval = currentTime - prevTime;
         const apy = interval > 0 && prevBalance_d18.gt(ZERO) ?
@@ -65,12 +73,11 @@ describe("Compound APY", function() {
 
         compoundResults.push({ balance: balanceStr, compRewardBalance: rewardStr, compPrice: priceStr, time: currentTime, interval, profit: profitStr, apy: apyStr });
         writeFile("compoundResults.json", JSON.stringify(compoundResults), err => { if (err) { console.log(err) } });
-
-
-
     });
 
     it("estimate USDC supply APY, Aave", async function() {
+        await ethers.provider.send("hardhat_impersonateAccount", [STRIPS_AAVE_WALLET]);
+        const stripsAaveSigner = await ethers.getSigner(STRIPS_AAVE_WALLET);
         const addressProvider = new ethers.Contract(Contracts.AaveLendingPoolAddressesProvider, aaveAbi["AaveLendingPoolAddressesProvider"], this.deployer);
         const lendingPoolAddress = await addressProvider.getLendingPool();
         const aaveLendingPool = new ethers.Contract(lendingPoolAddress, aaveLendingPoolAbi["abi"], this.deployer);
@@ -82,28 +89,27 @@ describe("Compound APY", function() {
 
         const lastResult = aaveResults.at(-1);
         const prevBalance_d18 = lastResult ? ethers.utils.parseUnits(lastResult.balance, 18) : ZERO;
-        const prevAaveReward_d18 = lastResult ? ethers.utils.parseUnits(lastResult.aaveRewardBalance, 18) : ZERO;
+        const prevAaveRewardBalance_d18 = lastResult ? ethers.utils.parseUnits(lastResult.aaveRewardBalance, 18) : ZERO;
         const prevTime = lastResult ? lastResult.time : 0;
 
-        const usdcBeforeWithdrawal = await this.USDC.balanceOf(STRIPS_TEST);
+        const usdcBeforeWithdrawal = await this.USDC.balanceOf(STRIPS_AAVE_WALLET);
 
-        const tx = await aaveLendingPool.connect(this.stripsSigner).withdraw(this.USDC.address, ethers.constants.MaxUint256, STRIPS_TEST);
+        const tx = await aaveLendingPool.connect(stripsAaveSigner).withdraw(this.USDC.address, ethers.constants.MaxUint256, STRIPS_AAVE_WALLET);
         const currentTime = Date.now();
         await tx.wait();
 
-        const usdcAfterWithdrawal = await this.USDC.balanceOf(STRIPS_TEST);
+        const usdcAfterWithdrawal = await this.USDC.balanceOf(STRIPS_AAVE_WALLET);
         const currentBalance = usdcAfterWithdrawal.sub(usdcBeforeWithdrawal);
         const currentBalance_d18 = currentBalance.mul(ethers.BigNumber.from(10).pow(12));
 
-        const currentAaveReward = await aaveIncentivesController.getRewardsBalance([Contracts.aaveAToken], STRIPS_TEST);
+        const currentAaveRewardBalance = await aaveIncentivesController.getRewardsBalance([Contracts.aaveAToken], STRIPS_AAVE_WALLET);
         const aavePriceData = await chainLinkAaveUSD.latestRoundData();
         const currentAavePrice = aavePriceData.answer;
-        const currentAaveReward_d18 = currentAaveReward.mul(currentAavePrice).div(ethers.BigNumber.from(10).pow(8));
-
-        const currentProfit = currentBalance_d18.sub(prevBalance_d18).add(currentAaveReward_d18.sub(prevAaveReward_d18));
+        const currentAaveRewardProfit_d18 = (currentAaveRewardBalance.sub(prevAaveRewardBalance_d18)).mul(currentAavePrice).div(ethers.BigNumber.from(10).pow(8));
+        const currentProfit = (currentBalance_d18.sub(prevBalance_d18)).add(currentAaveRewardProfit_d18);
         const profitStr = ethers.utils.formatUnits(currentProfit, 18);
         const balanceStr = ethers.utils.formatUnits(currentBalance_d18, 18);
-        const rewardStr = ethers.utils.formatUnits(currentAaveReward_d18, 18);
+        const aaveRewardBalanceStr = ethers.utils.formatUnits(currentAaveRewardBalance, 18);
         const priceStr = ethers.utils.formatUnits(currentAavePrice, 8);
         const interval = currentTime - prevTime;
         const apy = interval > 0 && prevBalance_d18.gt(ZERO) ?
@@ -111,7 +117,7 @@ describe("Compound APY", function() {
             ZERO;
         const apyStr = ethers.utils.formatUnits(apy, 18);
 
-        aaveResults.push({ balance: balanceStr, aaveRewardBalance: rewardStr, aavePrice: priceStr, time: currentTime, interval, profit: profitStr, apy: apyStr });
+        aaveResults.push({ balance: balanceStr, aaveRewardBalance: aaveRewardBalanceStr, aavePrice: priceStr, time: currentTime, interval, profit: profitStr, apy: apyStr });
         writeFile("aaveResults.json", JSON.stringify(aaveResults), err => { if (err) { console.log(err) } });
     });
 });
